dc0d04806ec9a95d21ac5853909f981195d411fc
Anjani <2387926@cognizant.com>
2025-02-25 10:57:32 +0530

    Initial commit

---
+++ OnlineFoodDeliverySystem/FoodDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineFoodDeliverySystem
+{
+    public class FoodDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
